10e13e03d175729fb454b7ee0be79f9331d8c647
Gennadii <g.mishchevskii@gmail.com>
2015-09-03 14:24:28 +0300

    Fixed 4:6 problem

--- office_time.py
+++ office_time.py
@@ -8,10 +8,10 @@ min_spent = int(spent[-2:]) + int(spent[:-3])*60
 min_now = int(now[-2:]) + int(now[:-3])*60
 
 time_to_work_int = 8*60 - min_spent
-time_to_work = str(time_to_work_int // 60) + ':' + str(time_to_work_int % 60)
+time_to_work = str(time_to_work_int // 60) + ':' + '0'*(time_to_work_int % 60 < 10) + str(time_to_work_int % 60)
 
 time_to_leave_int = min_now + time_to_work_int
-time_to_leave = str(time_to_leave_int // 60) + ':' + str(time_to_leave_int % 60)
+time_to_leave = str(time_to_leave_int // 60) + ':' + '0'*(time_to_leave_int % 60 < 10) + str(time_to_leave_int % 60)
 
 print('\n\n' + 'Time left to work: ' + time_to_work)
 input('\n\n' + 'Time to leave: ' + time_to_leave)
