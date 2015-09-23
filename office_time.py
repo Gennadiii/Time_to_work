a8d97f1126044d41b167a90373721b7011281011
Gennadii <g.mishchevskii@gmail.com>
2015-09-23 09:36:14 +0300

    JSON instead of pickle

--- office_time.py
+++ office_time.py
@@ -1,14 +1,14 @@
 from datetime import time
 from datetime import datetime
 import os.path
+import json
 
 direction = os.path.expanduser(r'~\Dropbox\Work\Python\Programms\txt\office_time.txt')
 spent = input('Input time You\'ve already been here: ')
 now = str(datetime.time(datetime.now()))[:5]
 additional_time = 0
 
-if spent[0] == '*':
-	spent = spent[1:]
+if '*' in spent:
 	additional_time = int(spent[:spent.find('*')])
 	spent = spent[spent.find('*')+1:]
 
@@ -16,20 +16,20 @@ if spent[-1] == '+':
 	spent = spent[:-1]
 	min_spent = int(spent[-2:]) + int(spent[:-3])*60
 	min_now = int(now[-2:]) + int(now[:-3])*60
-	time_of_coming = str(min_now - min_spent)
-	doc = open(direction,'w')
-	doc.write(time_of_coming)
-	doc.close()
+	time_of_coming = min_now - min_spent
+	data = {'additional_time':additional_time, 'time_of_coming':time_of_coming}
+	json.dump(data, open(direction, 'w'))
 
 min_spent = int(spent[-2:]) + int(spent[:-3])*60
 min_now = int(now[-2:]) + int(now[:-3])*60
 
+data = json.load(open(direction, 'r'))
+
+time_of_coming = data['time_of_coming']
+additional_time = data['additional_time']
+
 time_to_work_int = 8*60 + additional_time - min_spent
 time_to_work = str(time_to_work_int // 60) + ':' + '0'*(time_to_work_int % 60 < 10) + str(time_to_work_int % 60)
-
-doc = open(direction,'r')
-time_of_coming = int(doc.readline())
-doc.close()
 
 time_to_leave_int = min_now + time_to_work_int
 time_to_leave = str(time_to_leave_int // 60) + ':' + '0'*(time_to_leave_int % 60 < 10) + str(time_to_leave_int % 60)
