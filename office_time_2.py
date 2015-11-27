54fef0459f79978ad3989dd4453d81926b3753fc
Gennadii <g.mishchevskii@gmail.com>
2015-11-27 17:12:50 +0200

    Changed locators

--- office_time_2.py
+++ office_time_2.py
@@ -4,6 +4,19 @@ import json
 from selenium import webdriver
 from os.path import expanduser
 from selenium.common.exceptions import InvalidSelectorException
+from selenium.webdriver.common.action_chains import ActionChains
+
+today = 0
+yestarday = 1
+username = 'g.mishchevskyi'
+password = '*'
+
+password = password[:4] + password[-4:]
+
+password = chr(ord(password[0])-9) + chr(ord(password[1])+10) + chr(ord(password[2])-12) + chr(ord(password[3])-5) + chr(ord(password[4])) + chr(ord(password[5])+3) + chr(ord(password[6])-1) + chr(ord(password[7])-4)
+
+now = datetime.now()
+day_of_week = datetime.weekday(now)
 
 def time2int(time):
 	return int( time[:2] )*60 + int( time[3:] )
@@ -15,58 +28,50 @@ txt = expanduser(r'~\Dropbox\Work\Python\Programms\txt\office_time.txt')
 current_time_int = time2int( str(datetime.time(datetime.now()))[:5] )
 
 driver = webdriver.Chrome(expanduser(r'~\Dropbox\Work\Python\chromedriver.exe'))
-driver.get('https://portal-ua.globallogic.com/officetime/')
+driver.get('https://' + username + ':' + password + '@' + 'portal-ua.globallogic.com/officetime/')
 driver.implicitly_wait(600)
 
-for j in range(20):
-	try:
-		table_view = driver.find_element_by_xpath("//a[contains(text(), 'Table view')]")
-		table_view.click()
-		break
-	except InvalidSelectorException:
-		sleep(0.1)
-
-today = str(datetime.now().day) + '.' + str(datetime.now().month) + '.' + str(datetime.now().year)
-yestarday = str(datetime.now().day-1) + '.' + str(datetime.now().month) + '.' + str(datetime.now().year)
-
 def time_worked_for(day):
-	time_worked = driver.find_elements_by_xpath("//div[@data-day='" + day + "']")
-	time_worked = time_worked[1].text
-	no_teleport_int = time2int( time_worked[ time_worked.find(':')-2 : time_worked.find(':')+3 ] )
-	time_worked = time_worked[ time_worked.find(':')+1 : ]
-	teleport_int = time2int( time_worked[ time_worked.find(':')-2 : time_worked.find(':')+3 ] )
-	return no_teleport_int + teleport_int
+	chart = driver.find_element_by_css_selector("#bar > svg > rect:nth-child(" + str(25 + day_of_week - day) + ")")
+	ActionChains(driver).move_to_element(chart).perform()
+	time_worked_raw = driver.find_element_by_css_selector("#bar > div").text
+	time_worked = time_worked_raw[:5]
+	driver.quit()
+	return time2int(time_worked)
 
-time_worked = time_worked_for(today)
-time_of_coming = current_time_int - time_worked
+time_worked_int = time_worked_for(today)
+time_of_coming = current_time_int - time_worked_int
 
 data = json.load(open(txt, 'r'))
 
-if data['today'] != today:
-	worked_yestarday = time_worked_for(yestarday)
-	print( 'Yestarday worked: ' + int2time(worked_yestarday) )
+if data['day_of_week'] != day_of_week:
 	last_additional_time = data['additional_time']
+	if day_of_week != 0:
+		worked_yestarday = time_worked_for(yestarday)
+		print( 'Yestarday worked: ' + int2time(worked_yestarday) )
+	else:
+		worked_yestarday = 8*60 + last_additional_time
+
 	additional_time = last_additional_time + ( 8*60 - worked_yestarday )
 	if additional_time < 0: additional_time = 0
-	data = { 'today':today, 'time_of_coming':time_of_coming, 'additional_time':additional_time }
+	data = { 'day_of_week':day_of_week, 'time_of_coming':time_of_coming, 'additional_time':additional_time }
 	json.dump(data, open(txt, 'w'))
-
-driver.quit()
 
 data = json.load(open(txt, 'r'))
 
 time_of_coming = data['time_of_coming']
 additional_time = data['additional_time']
 
-time_to_work_int = 8*60 - time_worked + additional_time
+time_to_work_int = 8*60 - time_worked_int + additional_time
 time_to_work = int2time(time_to_work_int)
 
 time_to_leave_int = current_time_int + time_to_work_int
 time_to_leave = int2time(time_to_leave_int)
 
-fun_time_int = time_to_leave_int - time_of_coming - time_worked - time_to_work_int
+fun_time_int = time_to_leave_int - time_of_coming - time_worked_int - time_to_work_int
 fun_time = int2time(fun_time_int)
 
+print('\n\n' + 'Time worked: ' + int2time(time_worked_int) )
 print('\n\n' + 'Time left to work: ' + time_to_work)
 print('\n\n' + 'Time of fun: ' + fun_time)
 print('\n\n' + 'Time to leave: ' + time_to_leave + '\n\n')
