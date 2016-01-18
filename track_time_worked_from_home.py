299e5539f678f0e728d9e074e160a2d07f33bf7e
Gennadii <g.mishchevskii@gmail.com>
2016-01-18 17:33:00 +0200

    Create track_time_worked_from_home.py

---
+++ track_time_worked_from_home.py
@@ -0,0 +1,14 @@
+from time import sleep
+import json
+from os.path import expanduser
+
+txt = expanduser(r'~\Dropbox\Work\Python\Programms\txt\office_time.txt')
+
+time_worked = 0
+while True:
+	sleep(60)
+	data = json.load(open(txt, 'r'))
+	data['worked_from_home'] += 1
+	json.dump(data, open(txt, 'w'))
+	time_worked += 1
+	print( str(time_worked) + '\n' )
