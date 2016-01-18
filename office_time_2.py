2efcda860b40974da88407e5fef5f2f110b6b10b
Gennadii <g.mishchevskii@gmail.com>
2016-01-18 17:31:55 +0200

    Refactoring

--- office_time_2.py
+++ office_time_2.py
@@ -1,82 +1,126 @@
 from datetime import datetime
-from time import sleep
 import json
 from selenium import webdriver
 from os.path import expanduser
 from selenium.common.exceptions import InvalidSelectorException
 from selenium.webdriver.common.action_chains import ActionChains
-
-today = 0
-yestarday = 1
-username = 'g.mishchevskyi'
-password = '*'
-
-password = password[:4] + password[-4:]
-
-password = chr(ord(password[0])-9) + chr(ord(password[1])+10) + chr(ord(password[2])-12) + chr(ord(password[3])-5) + chr(ord(password[4])) + chr(ord(password[5])+3) + chr(ord(password[6])-1) + chr(ord(password[7])-4)
+from my_lib import *
 
 now = datetime.now()
 day_of_week = datetime.weekday(now)
 
-def time2int(time):
-	return int( time[:2] )*60 + int( time[3:] )
-
-def int2time(integer):
-	return str(integer // 60) + ':' + '0'*(integer % 60 < 10) + str(integer % 60)
+today = 0
+yestarday = 1
+username = '*'
+password = '*'
+days_length = 8*60
 
 txt = expanduser(r'~\Dropbox\Work\Python\Programms\txt\office_time.txt')
-current_time_int = time2int( str(datetime.time(datetime.now()))[:5] )
 
 driver = webdriver.Chrome(expanduser(r'~\Dropbox\Work\Python\chromedriver.exe'))
 driver.get('https://' + username + ':' + password + '@' + 'portal-ua.globallogic.com/officetime/')
 driver.implicitly_wait(30)
 
+def time2int(time):
+    return int( time[:2] )*60 + int( time[3:] )
+
+def int2time(integer):
+    return str(int(integer // 60)) + ':' + '0'*(integer % 60 < 10) + str(integer % 60)
+
 def time_worked_for(day):
-	chart = driver.find_element_by_css_selector("#bar > svg > rect:nth-child(" + str(25 + day_of_week - day) + ")")
-	ActionChains(driver).move_to_element(chart).perform()
-	time_worked_raw = driver.find_element_by_css_selector("#bar > div").text
-	time_worked = time_worked_raw[:5]
-	return time2int(time_worked)
+    chart = driver.find_element_by_css_selector("#bar > svg > rect:nth-child(" + str(25 + day_of_week - day) + ")")
+    ActionChains(driver).move_to_element(chart).perform()
+    time_worked_raw = driver.find_element_by_css_selector("#bar > div").text
+    time_worked = time_worked_raw[:5]
+    return time2int(time_worked)
 
-time_worked_int = time_worked_for(today)
-time_of_coming = current_time_int - time_worked_int
+def entered_first_time_today():
+    return data['day_of_week'] == day_of_week
 
-data = json.load(open(txt, 'r'))
+def json_load():
+    return json.load(open(txt, 'r'))
 
-if data['day_of_week'] != day_of_week:
-	last_additional_time = data['additional_time']
-	if day_of_week != 0:
-		worked_yestarday = time_worked_for(yestarday)
-		print( 'Yestarday worked: ' + int2time(worked_yestarday) )
-	else:
-		worked_yestarday = 8*60 + last_additional_time
+def json_dump():
+    json.dump(data, open(txt, 'w'))
 
-	additional_time = last_additional_time + ( 8*60 - worked_yestarday )
-	if additional_time < 0: additional_time = 0
-	data = { 'day_of_week':day_of_week, 'time_of_coming':time_of_coming, 'additional_time':additional_time }
-	json.dump(data, open(txt, 'w'))
-	
-driver.quit()
+def today_is_not_Monday():
+    return day_of_week != 0
+
+def print_time_worked_yestarday():
+    print( '\n' + 'Yestarday worked: ' + int2time(worked_yestarday) )
+
+def print_time_worked():
+    print('\n\n' + 'Time worked: ' + int2time(time_worked) )
+
+def print_time_to_work(time_to_work):
+    if time_to_work <= 0: time_to_work = 'Go home! )'
+    print('\n\n' + 'Time left to work: ' + int2time(time_to_work))
+
+def print_fun_time():
+    print('\n\n' + 'Time of fun: ' + int2time(fun_time))
+
+def print_time_to_leave():
+    print('\n\n' + 'Time to leave: ' + int2time(time_to_leave))
+
+def print_additional_time():
+    print('\n\n' + 'Additional time: ' + int2time(data['additional_time']))
+    
+current_time = time2int( str(datetime.time(datetime.now()))[:5] )
+time_worked = time_worked_for(today)
+time_of_coming = current_time - time_worked
+
+data = json_load()
+
+if not entered_first_time_today():
+    driver.quit()
+else:
+    if today_is_not_Monday():
+        worked_yestarday = time_worked_for(yestarday)
+        driver.quit()
+        print('Waiting 5 minutes for dropbox to update...')
+        sleep(5*60)
+
+        worked_yestarday += data['worked_from_home']
+        print_time_worked_yestarday()
+        additional_time = data['additional_time'] + ( days_length - worked_yestarday )
+    else:
+        driver.quit()
+        worked_yestarday = days_length
+        additional_time = 0
+
+    data['worked_from_home'] = 0
+    json_dump()
+
 
 time_of_coming = data['time_of_coming']
 additional_time = data['additional_time']
+day_of_week = data['day_of_week']
 
-time_to_work_int = 8*60 - time_worked_int + additional_time
-if time_to_work_int <= 0:
-	time_to_work = 'Go home! )'
-else:
-	time_to_work = int2time(time_to_work_int)
+time_to_work = days_length - time_worked + data['additional_time'] + data['fun_time']
 
-time_to_leave_int = current_time_int + time_to_work_int
-time_to_leave = int2time(time_to_leave_int)
+time_to_leave = current_time + time_to_work
 
-fun_time_int = time_to_leave_int - time_of_coming - time_worked_int - time_to_work_int
-fun_time = int2time(fun_time_int)
+fun_time = time_to_leave - time_of_coming - time_worked - time_to_work + data['fun_time']
 
-print('\n\n' + 'Time worked: ' + int2time(time_worked_int) )
-print('\n\n' + 'Time left to work: ' + time_to_work)
-print('\n\n' + 'Time of fun: ' + fun_time)
-print('\n\n' + 'Time to leave: ' + time_to_leave + '\n\n')
-if additional_time != 0: print( '\n\n' + 'Additional_time : ' + str(additional_time) )
+print_time_worked()
+print_time_to_work(time_to_work)
+print_fun_time()
+print_time_to_leave()
+print_additional_time()
+
+add_time = input()
+
+if len(add_time) == 0: exit()
+add_time = int(add_time)
+
+data['additional_time'] += add_time
+json_dump()
+
+time_to_work += add_time
+time_to_leave += add_time
+
+print_time_to_work(time_to_work)
+print_time_to_leave()
+print_additional_time()
 
 input()
