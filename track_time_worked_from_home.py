d8233451400b8da099c61b60ed2317464647cf3c
Gennadii <g.mishchevskii@gmail.com>
2016-01-19 09:17:38 +0200

    Refactoring

--- track_time_worked_from_home.py
+++ track_time_worked_from_home.py
@@ -2,12 +2,12 @@ from my_lib import *
 import json
 
 txt = expanduser(r'~\Dropbox\Work\Python\Programms\txt\office_time.txt')
+data = json.load(open(txt, 'r'))
 
 time_worked = 0
 while True:
-	sleep(60)
-	data = json.load(open(txt, 'r'))
-	data['worked_from_home'] += 1
-	json.dump(data, open(txt, 'w'))
-	time_worked += 1
 	print( str(time_worked) + '\n' )
+	sleep(60)
+	time_worked += 1
+	data['worked_from_home'] = time_worked
+	json.dump(data, open(txt, 'w'))
